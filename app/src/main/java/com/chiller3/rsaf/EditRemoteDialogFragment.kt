f167884f362f7425d95947faf3421f5310a0b6c9
Andrew Gunnerson <accounts+github@chiller3.com>
2023-06-20 01:03:32 -0400

    Add option to show all dialogs at the bottom of the screen
    This makes it easier to reach the UI elements one-handed and prevents
    the dialog action buttons from shifting.
    
    Fixes: #9
    
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- app/src/main/java/com/chiller3/rsaf/EditRemoteDialogFragment.kt
+++ app/src/main/java/com/chiller3/rsaf/EditRemoteDialogFragment.kt
@@ -3,6 +3,7 @@ package com.chiller3.rsaf
 import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
+import android.view.Gravity
 import androidx.core.os.bundleOf
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
@@ -43,6 +44,11 @@ open class EditRemoteDialogFragment : DialogFragment() {
                 dismiss()
             }
             .create()
+            .apply {
+                if (Preferences(requireContext()).dialogsAtBottom) {
+                    window!!.attributes.gravity = Gravity.BOTTOM
+                }
+            }
     }
 
     override fun onDismiss(dialog: DialogInterface) {
