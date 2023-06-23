2b10b60233c29091e7eaa1cd3375f2a7ce78291b
Andrew Gunnerson <accounts+github@chiller3.com>
2023-06-23 17:18:29 -0400

    Update kotlin and fragment-ktx dependencies
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -186,7 +186,7 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.7.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.fragment:fragment-ktx:1.5.7")
+    implementation("androidx.fragment:fragment-ktx:1.6.0")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     implementation("com.google.android.material:material:1.9.0")
