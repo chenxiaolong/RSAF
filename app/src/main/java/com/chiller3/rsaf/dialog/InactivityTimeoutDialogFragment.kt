chenxiaolong/RSAF

commit 6807ad0d0e959c84bd3edead2d29c0b2d44de189

Add password confirmation when exporting the config
Otherwise, it's too easy to not notice a typo and end up with an
unusable config.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/rsaf/dialog/TextInputDialogFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -9,75 +9,108 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.text.InputType
-import android.view.Gravity
 import androidx.appcompat.app.AlertDialog
 import androidx.core.os.bundleOf
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
-import com.chiller3.rsaf.Preferences
 import com.chiller3.rsaf.databinding.DialogTextInputBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-open class TextInputDialogFragment : DialogFragment() {
+private const val ARG_TITLE = "title"
+private const val ARG_MESSAGE = "message"
+private const val ARG_HINT = "hint"
+private const val ARG_CONFIRM_HINT = "confirm_hint"
+private const val ARG_INPUT_TYPE = "input_type"
+private const val ARG_ORIG_VALUE = "orig_value"
+
+enum class TextInputType {
+    NORMAL,
+    PASSWORD,
+    NUMBER,
+}
+
+data class TextInputParams(
+    val inputType: TextInputType,
+    val title: String,
+    val message: String,
+    val hint: String,
+    val confirmHint: String? = null,
+    val origValue: String? = null,
+) {
+    constructor(args: Bundle) : this(
+        inputType = TextInputType.entries[args.getInt(ARG_INPUT_TYPE)],
+        title = args.getString(ARG_TITLE)!!,
+        message = args.getString(ARG_MESSAGE)!!,
+        hint = args.getString(ARG_HINT)!!,
+        confirmHint = args.getString(ARG_CONFIRM_HINT),
+        origValue = args.getString(ARG_ORIG_VALUE),
+    )
+
+    fun toArgs() = bundleOf(
+        ARG_INPUT_TYPE to inputType.ordinal,
+        ARG_TITLE to title,
+        ARG_MESSAGE to message,
+        ARG_HINT to hint,
+        ARG_CONFIRM_HINT to confirmHint,
+        ARG_ORIG_VALUE to origValue,
+    )
+}
+
+abstract class TextInputDialogFragment<T> : DialogFragment() {
     companion object {
-        private const val ARG_TITLE = "title"
-        private const val ARG_MESSAGE = "message"
-        private const val ARG_HINT = "hint"
-        private const val ARG_IS_PASSWORD = "is_password"
-        const val RESULT_SUCCESS = "success"
-        const val RESULT_INPUT = "input"
-
-        @JvmStatic
-        protected fun toArgs(title: String, message: String, hint: String, isPassword: Boolean) =
-            bundleOf(
-                ARG_TITLE to title,
-                ARG_MESSAGE to message,
-                ARG_HINT to hint,
-                ARG_IS_PASSWORD to isPassword,
-            )
-
-        fun newInstance(title: String, message: String, hint: String, isPassword: Boolean):
-                TextInputDialogFragment =
-            TextInputDialogFragment().apply {
-                arguments = toArgs(title, message, hint, isPassword)
-            }
+        protected const val RESULT_SUCCESS = "success"
+        protected const val RESULT_ORIG_VALUE = "orig_value"
+        protected const val RESULT_VALUE = "value"
     }
 
     private lateinit var binding: DialogTextInputBinding
+    private lateinit var params: TextInputParams
     private var success: Boolean = false
-    private var input: String = ""
+    private var value: T? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        val arguments = requireArguments()
+        params = TextInputParams(requireArguments())
 
         binding = DialogTextInputBinding.inflate(layoutInflater)
-        binding.message.text = arguments.getString(ARG_MESSAGE)
-        binding.textLayout.hint = arguments.getString(ARG_HINT)
-        binding.text.inputType = InputType.TYPE_CLASS_TEXT or
-            if (arguments.getBoolean(ARG_IS_PASSWORD)) {
-                InputType.TYPE_TEXT_VARIATION_PASSWORD
-            } else {
-                0
-            }
+        binding.message.text = params.message
+
+        binding.textLayout.hint = params.hint
+        binding.confirmTextLayout.hint = params.confirmHint
+        binding.confirmTextLayout.isVisible = params.confirmHint != null
+
+        val inputType = when (params.inputType) {
+            TextInputType.NORMAL ->
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_NO_SUGGESTIONS
+            TextInputType.PASSWORD ->
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            TextInputType.NUMBER -> InputType.TYPE_CLASS_NUMBER
+        }
+        binding.text.inputType = inputType
+        binding.confirmText.inputType = inputType
+
         binding.text.addTextChangedListener {
+            value = translateInput(it.toString())
+
+            refreshOkButtonEnabledState()
+        }
+        binding.confirmText.addTextChangedListener {
             refreshOkButtonEnabledState()
         }
 
+        if (savedInstanceState == null) {
+            binding.text.setText(params.origValue)
+        }
+
         return MaterialAlertDialogBuilder(requireContext())
-            .setTitle(arguments.getString(ARG_TITLE))
+            .setTitle(params.title)
             .setView(binding.root)
             .setPositiveButton(android.R.string.ok) { _, _ ->
                 success = true
-                input = binding.text.text.toString()
             }
             .setNegativeButton(android.R.string.cancel, null)
             .create()
-            .apply {
-                if (Preferences(requireContext()).dialogsAtBottom) {
-                    window!!.attributes.gravity = Gravity.BOTTOM
-                }
-            }
     }
 
     override fun onStart() {
@@ -90,14 +123,18 @@ open class TextInputDialogFragment : DialogFragment() {
 
         setFragmentResult(tag!!, bundleOf(
             RESULT_SUCCESS to success,
-            RESULT_INPUT to input,
-        ))
+            RESULT_ORIG_VALUE to params.origValue,
+            RESULT_VALUE to value,
+        ).also(::updateResult))
     }
 
     private fun refreshOkButtonEnabledState() {
-        (dialog as AlertDialog).getButton(AlertDialog.BUTTON_POSITIVE).isEnabled =
-            isValid(binding.text.text.toString())
+        (dialog as AlertDialog?)?.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = value != null
+                && (params.confirmHint == null
+                || binding.text.text?.toString() == binding.confirmText.text?.toString())
     }
 
-    open fun isValid(input: String): Boolean = true
+    abstract fun translateInput(input: String): T?
+
+    open fun updateResult(result: Bundle) {}
 }

app/src/androidTest/java/com/chiller3/rsaf/ImportExportTest.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -92,7 +92,7 @@ class ImportExportTest {
     @Test
     fun testPlainText() {
         val outputStream = ByteArrayOutputStream()
-        RcloneConfig.exportConfiguration(outputStream, "")
+        RcloneConfig.exportConfiguration(outputStream, RcloneConfig.Password(""))
 
         val config = parseConfig(outputStream.toString(Charsets.UTF_8))
         assertTrue(config.containsKey(remote))
@@ -102,7 +102,7 @@ class ImportExportTest {
         RcloneRpc.deleteRemote(remote)
 
         val inputStream = ByteArrayInputStream(outputStream.toByteArray())
-        RcloneConfig.importConfiguration(inputStream, "")
+        RcloneConfig.importConfiguration(inputStream, RcloneConfig.Password(""))
 
         assertTrue(RcloneRpc.remoteNames.contains(remote))
     }
@@ -110,7 +110,7 @@ class ImportExportTest {
     @Test
     fun testEncrypted() {
         val outputStream = ByteArrayOutputStream()
-        RcloneConfig.exportConfiguration(outputStream, "test")
+        RcloneConfig.exportConfiguration(outputStream, RcloneConfig.Password("test"))
 
         val rawConfig = outputStream.toString(Charsets.UTF_8)
         assertTrue(isEncryptedConfig(rawConfig))
@@ -119,12 +119,12 @@ class ImportExportTest {
 
         assertThrows(RcloneConfig.BadPasswordException::class.java) {
             val inputStream = ByteArrayInputStream(outputStream.toByteArray())
-            RcloneConfig.importConfiguration(inputStream, "wrong")
+            RcloneConfig.importConfiguration(inputStream, RcloneConfig.Password("wrong"))
         }
         assertFalse(RcloneRpc.remoteNames.contains(remote))
 
         val inputStream = ByteArrayInputStream(outputStream.toByteArray())
-        RcloneConfig.importConfiguration(inputStream, "test")
+        RcloneConfig.importConfiguration(inputStream, RcloneConfig.Password("test"))
         assertTrue(RcloneRpc.remoteNames.contains(remote))
     }
 }

app/src/main/java/com/chiller3/rsaf/backup/RcloneConfigBackupHelper.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -57,7 +57,7 @@ class RcloneConfigBackupHelper : BackupHelper {
     ) {
         try {
             val payloadStream = ByteArrayOutputStream()
-            RcloneConfig.exportConfiguration(payloadStream, "")
+            RcloneConfig.exportConfiguration(payloadStream, RcloneConfig.Password(""))
 
             val payload = payloadStream.toByteArray()
             val oldPayloadHash = oldState?.let { readHashFromState(it) }
@@ -86,7 +86,7 @@ class RcloneConfigBackupHelper : BackupHelper {
         Log.i(TAG, "Restoring ${data.size()} byte payload")
 
         try {
-            RcloneConfig.importConfiguration(EntityInputStream(data), "")
+            RcloneConfig.importConfiguration(EntityInputStream(data), RcloneConfig.Password(""))
             Log.i(TAG, "Successfully restored rclone configuration")
         } catch (e: Exception) {
             Log.e(TAG, "Failed to restore rclone configuration", e)

app/src/main/java/com/chiller3/rsaf/dialog/InteractiveConfigurationDialogFragment.kt
@@ -67,7 +67,7 @@ class InteractiveConfigurationDialogFragment : DialogFragment() {
 
             if (isPassword) {
                 try {
-                    value = RcloneConfig.revealPassword(text)
+                    value = RcloneConfig.revealPassword(text).value
                 } catch (e: ErrnoException) {
                     Log.w(TAG, "Failed to reveal password", e)
                 }

app/src/main/java/com/chiller3/rsaf/dialog/PasswordDialogFragment.kt
@@ -1,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Andrew Gunnerson
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package com.chiller3.rsaf.dialog
+
+import android.content.Context
+import com.chiller3.rsaf.R
+import com.chiller3.rsaf.settings.ImportExportMode
+
+class PasswordDialogFragment : TextInputDialogFragment<String>() {
+    companion object {
+        val TAG: String = PasswordDialogFragment::class.java.simpleName
+
+        const val RESULT_SUCCESS = TextInputDialogFragment.RESULT_SUCCESS
+        const val RESULT_PASSWORD = RESULT_VALUE
+
+        fun newInstance(context: Context, mode: ImportExportMode) =
+            PasswordDialogFragment().apply {
+                arguments = TextInputParams(
+                    inputType = TextInputType.PASSWORD,
+                    title = when (mode) {
+                        ImportExportMode.IMPORT ->
+                            context.getString(R.string.dialog_import_password_title)
+                        ImportExportMode.EXPORT ->
+                            context.getString(R.string.dialog_export_password_title)
+                    },
+                    message = when (mode) {
+                        ImportExportMode.IMPORT ->
+                            context.getString(R.string.dialog_import_password_message)
+                        ImportExportMode.EXPORT ->
+                            context.getString(R.string.dialog_export_password_message)
+                    },
+                    hint = when (mode) {
+                        ImportExportMode.IMPORT ->
+                            context.getString(R.string.dialog_import_password_hint)
+                        ImportExportMode.EXPORT ->
+                            context.getString(R.string.dialog_export_password_hint)
+                    },
+                    confirmHint = when (mode) {
+                        ImportExportMode.IMPORT -> null
+                        ImportExportMode.EXPORT ->
+                            context.getString(R.string.dialog_export_password_confirm_hint)
+                    },
+                ).toArgs()
+            }
+    }
+
+    override fun translateInput(input: String): String = input
+}

app/src/main/java/com/chiller3/rsaf/dialog/RemoteNameDialogFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -30,23 +30,26 @@ sealed interface RemoteNameDialogAction {
     }
 }
 
-class RemoteNameDialogFragment : TextInputDialogFragment() {
+class RemoteNameDialogFragment : TextInputDialogFragment<String>() {
     companion object {
         private const val ARG_REMOTE_NAMES = "blacklist"
         const val RESULT_SUCCESS = TextInputDialogFragment.RESULT_SUCCESS
-        const val RESULT_INPUT = TextInputDialogFragment.RESULT_INPUT
+        const val RESULT_NAME = RESULT_VALUE
 
-        fun newInstance(context: Context, action: RemoteNameDialogAction, remoteNames: Array<String>) =
-            RemoteNameDialogFragment().apply {
-                arguments = toArgs(
-                    action.getTitle(context),
-                    context.getString(R.string.dialog_remote_name_message),
-                    context.getString(R.string.dialog_remote_name_hint),
-                    false,
-                ).apply {
-                    putStringArray(ARG_REMOTE_NAMES, remoteNames)
-                }
+        fun newInstance(
+            context: Context,
+            action: RemoteNameDialogAction,
+            remoteNames: Array<String>,
+        ) = RemoteNameDialogFragment().apply {
+            arguments = TextInputParams(
+                inputType = TextInputType.NORMAL,
+                title = action.getTitle(context),
+                message = context.getString(R.string.dialog_remote_name_message),
+                hint = context.getString(R.string.dialog_remote_name_hint),
+            ).toArgs().apply {
+                putStringArray(ARG_REMOTE_NAMES, remoteNames)
             }
+        }
     }
 
     private lateinit var remoteNames: Array<String>
@@ -56,11 +59,16 @@ class RemoteNameDialogFragment : TextInputDialogFragment() {
         return super.onCreateDialog(savedInstanceState)
     }
 
-    override fun isValid(input: String): Boolean =
+    override fun translateInput(input: String): String? {
         try {
             RcloneConfig.checkName(input)
-            input !in remoteNames
+            if (input !in remoteNames) {
+                return input
+            }
         } catch (_: Exception) {
-            false
+            // Ignore
         }
+
+        return null
+    }
 }

app/src/main/java/com/chiller3/rsaf/rclone/RcloneConfig.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023-2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -24,6 +24,10 @@ import kotlin.io.path.inputStream
 import kotlin.io.path.outputStream
 
 object RcloneConfig {
+    data class Password(val value: String) {
+        override fun toString(): String = "<password>"
+    }
+
     private val TAG = RcloneConfig::class.java.simpleName
 
     const val FILENAME = "rclone.conf"
@@ -44,6 +48,7 @@ object RcloneConfig {
      * Rclone's configuration system is all global state. Anything that touches the configuration
      * must do so with the mutex locked.
      */
+    @Suppress("PLATFORM_CLASS_MAPPED_TO_KOTLIN")
     private val globalStateLock = Object()
 
     /** Initialize global config state and load config file. */
@@ -84,28 +89,28 @@ object RcloneConfig {
      * hardware keystore itself. If the password has not yet been generated, a new 128-byte random
      * password is generated and stored.
      */
-    private val hardwareWrappedPassword: String
+    private val hardwareWrappedPassword: Password
         get() {
             synchronized(passwordStore) {
                 if (passwordStore.password == null) {
                     passwordStore.password = RandomUtils.generatePassword(128)
                 }
 
-                return passwordStore.password!!
+                return Password(passwordStore.password!!)
             }
         }
 
-    private fun setConfigLocked(path: String, password: String) {
+    private fun setConfigLocked(path: String, password: Password) {
         val error = RbError()
 
         if (!Rcbridge.rbConfigSetPath(path, error)) {
             throw error.toException("rbConfigSetPath")
         }
 
-        if (password.isEmpty()) {
+        if (password.value.isEmpty()) {
             Rcbridge.rbConfigClearPassword()
         } else {
-            if (!Rcbridge.rbConfigSetPassword(password, error)) {
+            if (!Rcbridge.rbConfigSetPassword(password.value, error)) {
                 throw error.toException("rbConfigSetPassword")
             }
         }
@@ -141,7 +146,7 @@ object RcloneConfig {
         notifyConfigChanged()
     }
 
-    fun importConfiguration(input: InputStream, password: String) {
+    fun importConfiguration(input: InputStream, password: Password) {
         withTempFile(applicationContext, "import.conf") { tempConfig ->
             tempConfig.outputStream().use { out ->
                 input.copyTo(out)
@@ -165,7 +170,7 @@ object RcloneConfig {
         }
     }
 
-    fun importConfigurationUri(uri: Uri, password: String) {
+    fun importConfigurationUri(uri: Uri, password: Password) {
         val input = applicationContext.contentResolver.openInputStream(uri)
             ?: throw IOException("Failed to open for reading: $uri")
 
@@ -174,7 +179,7 @@ object RcloneConfig {
         }
     }
 
-    fun exportConfiguration(output: OutputStream, password: String) {
+    fun exportConfiguration(output: OutputStream, password: Password) {
         withTempFile(applicationContext, "export.conf") { tempConfig ->
             synchronized(globalStateLock) {
                 try {
@@ -191,7 +196,7 @@ object RcloneConfig {
         }
     }
 
-    fun exportConfigurationUri(uri: Uri, password: String) {
+    fun exportConfigurationUri(uri: Uri, password: Password) {
         val output = applicationContext.contentResolver.openOutputStream(uri, "wt")
             ?: throw IOException("Failed to open for writing: $uri")
 
@@ -224,11 +229,11 @@ object RcloneConfig {
     class BadPasswordException(message: String?, cause: Throwable? = null)
         : Exception(message, cause)
 
-    fun revealPassword(obscured: String): String {
+    fun revealPassword(obscured: String): Password {
         val error = RbError()
         val result = Rcbridge.rbPasswordReveal(obscured, error)
             ?: throw error.toException("rbPasswordReveal")
 
-        return result.plainText
+        return Password(result.plainText)
     }
 }

app/src/main/java/com/chiller3/rsaf/settings/EditRemoteFragment.kt
@@ -193,14 +193,14 @@ class EditRemoteFragment : PreferenceBaseFragment(), FragmentResultListener,
         when (requestKey) {
             TAG_RENAME_REMOTE -> {
                 if (bundle.getBoolean(RemoteNameDialogFragment.RESULT_SUCCESS)) {
-                    val newRemote = bundle.getString(RemoteNameDialogFragment.RESULT_INPUT)!!
+                    val newRemote = bundle.getString(RemoteNameDialogFragment.RESULT_NAME)!!
 
                     viewModel.renameRemote(newRemote)
                 }
             }
             TAG_DUPLICATE_REMOTE -> {
                 if (bundle.getBoolean(RemoteNameDialogFragment.RESULT_SUCCESS)) {
-                    val newRemote = bundle.getString(RemoteNameDialogFragment.RESULT_INPUT)!!
+                    val newRemote = bundle.getString(RemoteNameDialogFragment.RESULT_NAME)!!
 
                     viewModel.duplicateRemote(newRemote)
                 }

commit 692716d606be1f250f9d2fa74c3e21f136839c02

InactivityTimeoutDialogFragment: Hide second text box
Regression introduced in 6807ad0d0e959c84bd3edead2d29c0b2d44de189.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/rsaf/dialog/InactivityTimeoutDialogFragment.kt
@@ -12,6 +12,7 @@ import android.os.Bundle
 import android.text.InputType
 import android.view.Gravity
 import androidx.appcompat.app.AlertDialog
+import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.setFragmentResult
@@ -37,6 +38,9 @@ class InactivityTimeoutDialogFragment : DialogFragment() {
 
         binding = DialogTextInputBinding.inflate(layoutInflater)
         binding.message.text = getString(R.string.dialog_inactivity_timeout_message)
+
+        binding.confirmTextLayout.isVisible = false
+
         binding.text.inputType = InputType.TYPE_CLASS_NUMBER
         binding.text.addTextChangedListener {
             duration = try {

app/src/main/java/com/chiller3/rsaf/dialog/TextInputDialogFragment.kt
@@ -128,7 +128,7 @@ abstract class TextInputDialogFragment<T> : DialogFragment() {
     private fun refreshOkButtonEnabledState() {
         (dialog as AlertDialog?)?.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = value != null
                 && (params.confirmHint == null
-                || binding.text.text?.toString() == binding.confirmText.text?.toString())
+                        || binding.text.text?.toString() == binding.confirmText.text?.toString())
     }
 
     abstract fun translateInput(input: String): T?