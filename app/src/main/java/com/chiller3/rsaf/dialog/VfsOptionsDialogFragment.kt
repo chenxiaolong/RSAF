chenxiaolong/RSAF

commit 42e18b38d1cb67cdb0302220f331850eb373d6d4

Add support for setting custom VFS options
The old "VFS caching" option that switches between vfs_cache_mode=writes
and vfs_cache_mode=off is now replaced with a new mechanism that allows
setting arbitrary VFS options. All options aside from vfs_write_back are
supported.

With this in place, it is now possible to set vfs_cache_mode=full, which
requires a complete reworking of how RSAF keeps the process alive.
Previously, there was:

* OpenFilesService, which kept the app alive while a file was open or
  being synchronously uploaded in close()
* BackgroundUploadMonitorService, which kept the app alive during
  asynchronous uploads following an app crash or termination

The latter relied on polling open fds to files within the VFS cache
directory and reporting upload progress based on the current file
offset. With vfs_cache_mode=full, we can no longer assume that all these
fds are for uploads, so the feature for guessing file upload progress
has been removed. Now, we just show how many in progress and pending
uploads there are, which we get by polling the vfs/queue RPC call.

To simplify the code, the two services are now combined into a single
KeepAliveService, which keeps track of both open files and asynchronous
uploads. Also, we now explicitly wait for the VFS cache cleanup to run
at least once instead of blindly hoping that it will happen before
Android SIGSTOPs us. After all ongoing operations are complete, the
service will wait for the largest vfs_cache_max_age +
vfs_cache_poll_interval value out of all active VFS instances + a buffer
of 5 seconds before exiting.

Finally, unlike the previous hacky mechanism where we could potentially
keep a caching VFS and streaming VFS alive at the same time, we now
require the user to reload the VFS when changing any VFS options. This
is simpler and safer, but will interrupt ongoing file operations. The
custom VFS options dialog makes the user aware of this and allows them
to choose whether to reload.

Fixes: #190

app/src/main/java/com/chiller3/rsaf/dialog/VfsOptionsDialogFragment.kt
@@ -1,0 +1,206 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Andrew Gunnerson
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package com.chiller3.rsaf.dialog
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
+import android.os.Bundle
+import android.system.ErrnoException
+import android.text.Annotation
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.SpannedString
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.Gravity
+import android.view.View
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import androidx.core.net.toUri
+import androidx.core.os.bundleOf
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.setFragmentResult
+import com.chiller3.rsaf.Preferences
+import com.chiller3.rsaf.R
+import com.chiller3.rsaf.binding.rcbridge.Rcbridge
+import com.chiller3.rsaf.databinding.DialogVfsOptionsBinding
+import com.chiller3.rsaf.extension.toSingleLineString
+import com.chiller3.rsaf.rclone.RcloneRpc
+import com.chiller3.rsaf.rclone.VfsCache
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class VfsOptionsDialogFragment : DialogFragment() {
+    companion object {
+        val TAG: String = VfsOptionsDialogFragment::class.java.simpleName
+
+        private const val ARG_REMOTE = "remote"
+        const val RESULT_REMOTE = ARG_REMOTE
+
+        fun newInstance(remote: String): VfsOptionsDialogFragment =
+            VfsOptionsDialogFragment().apply {
+                arguments = bundleOf(ARG_REMOTE to remote)
+            }
+    }
+
+    private lateinit var binding: DialogVfsOptionsBinding
+    private lateinit var remote: String
+    private lateinit var config: RcloneRpc.RemoteConfig
+    private var overrides: Map<String, String>? = null
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val arguments = requireArguments()
+        remote = arguments.getString(ARG_REMOTE)!!
+        config = RcloneRpc.remoteConfigs[remote]!!
+
+        binding = DialogVfsOptionsBinding.inflate(layoutInflater)
+
+        binding.message.movementMethod = LinkMovementMethod.getInstance()
+        binding.message.text = buildMessage()
+
+        binding.text.addTextChangedListener {
+            try {
+                val newOverrides = mutableMapOf<String, String>()
+
+                for (line in it.toString().splitToSequence('\n')) {
+                    if (line.trim().isEmpty()) {
+                        continue
+                    }
+
+                    val pieces = line.split('=', limit = 2)
+
+                    // Treat an incomplete line as just the key. rcbridge will show a better error
+                    // message for unknown keys.
+                    newOverrides[pieces[0]] = if (pieces.size > 1) {
+                        pieces[1]
+                    } else {
+                        ""
+                    }
+                }
+
+                VfsCache.getVfsOptions(newOverrides)
+                overrides = newOverrides
+
+                binding.textLayout.error = null
+                // Don't keep the layout space for the error message reserved.
+                binding.textLayout.isErrorEnabled = false
+            } catch (e: Exception) {
+                overrides = null
+
+                binding.textLayout.error = if (e is ErrnoException) {
+                    e.cause!!.message
+                } else {
+                    e.toSingleLineString()
+                }
+            }
+
+            refreshButtonsEnabledState()
+        }
+
+        if (savedInstanceState == null) {
+            binding.text.setText(buildString {
+                for ((key, value) in config.vfsOptions) {
+                    if (isNotEmpty()) {
+                        append('\n')
+                    }
+                    append("$key=$value")
+                }
+            })
+        }
+
+        isCancelable = false
+
+        val dialog = MaterialAlertDialogBuilder(requireContext())
+            .setTitle(getString(R.string.vfs_options_title, remote))
+            .setView(binding.root)
+            .setPositiveButton(R.string.vfs_options_save_and_reload) { _, _ ->
+                save(true)
+            }
+            .setNegativeButton(android.R.string.cancel, null)
+            .setNeutralButton(R.string.vfs_options_save_only) { _, _ ->
+                save(false)
+            }
+            .create()
+            .apply {
+                if (Preferences(requireContext()).dialogsAtBottom) {
+                    window!!.attributes.gravity = Gravity.BOTTOM
+                }
+
+                // The dialog can resize due to the multiline input. Make sure the dialog doesn't
+                // get hidden behind the IME.
+                //
+                // SOFT_INPUT_ADJUST_RESIZE works reliably, but is deprecated. The normal way of
+                // using setOnApplyWindowInsetsListener() + adjusting padding does not work for the
+                // dialog's DecorView. It causes terrible layout issues.
+                //
+                // Adding WindowInsetsCompat.Type.ime() to window!!.attributes.fitInsetsTypes does
+                // not work in all cases either. It kind of works until you add enough lines to max
+                // out the height, rotate to landscape, rotate back to portrait, and then tap on the
+                // text box. The dialog doesn't resize to the correct size until the keyboard is
+                // closed and reopened.
+                //
+                // AOSP still uses this in several places, like the Settings app, so it should stay
+                // working.
+                @Suppress("DEPRECATION")
+                window!!.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+
+                setCanceledOnTouchOutside(false)
+            }
+
+        return dialog
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        super.onDismiss(dialog)
+
+        // Just to signal completion to the parent.
+        setFragmentResult(tag!!, bundleOf(RESULT_REMOTE to remote))
+    }
+
+    private fun buildMessage(): SpannableStringBuilder {
+        val origMessage = getText(R.string.vfs_options_message) as SpannedString
+        val message = SpannableStringBuilder(origMessage)
+        val annotations = message.getSpans(0, origMessage.length, Annotation::class.java)
+
+        for (annotation in annotations) {
+            val start = message.getSpanStart(annotation)
+            val end = message.getSpanEnd(annotation)
+
+            if (annotation.key == "type" && annotation.value == "rclone_vfs_docs") {
+                message.setSpan(
+                    object : ClickableSpan() {
+                        override fun onClick(widget: View) {
+                            val uri = "https://rclone.org/commands/rclone_mount/".toUri()
+                            startActivity(Intent(Intent.ACTION_VIEW, uri))
+                        }
+                    },
+                    start,
+                    end,
+                    Spannable.SPAN_EXCLUSIVE_EXCLUSIVE,
+                )
+            } else {
+                throw IllegalStateException("Invalid annotation: $annotation")
+            }
+        }
+
+        return message
+    }
+
+    private fun refreshButtonsEnabledState() {
+        val dialog = dialog as AlertDialog?
+        dialog?.getButton(AlertDialog.BUTTON_POSITIVE)?.isEnabled = overrides != null
+        dialog?.getButton(AlertDialog.BUTTON_NEUTRAL)?.isEnabled = overrides != null
+    }
+
+    private fun save(reload: Boolean) {
+        RcloneRpc.setRemoteConfig(remote, config.copy(vfsOptions = overrides!!))
+
+        if (reload) {
+            Rcbridge.rbCacheClearRemote("$remote:", false)
+        }
+    }
+}

app/src/main/java/com/chiller3/rsaf/rclone/RcloneRpc.kt
@@ -5,30 +5,26 @@
 
 package com.chiller3.rsaf.rclone
 
-import android.util.Log
 import com.chiller3.rsaf.binding.rcbridge.Rcbridge
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 
 object RcloneRpc {
-    private val TAG = RcloneRpc::class.java.simpleName
-
     private const val CUSTOM_OPT_PREFIX = "rsaf:"
     // This is called hidden due to backwards compatibility.
     private const val CUSTOM_OPT_HARD_BLOCKED = CUSTOM_OPT_PREFIX + "hidden"
     private const val CUSTOM_OPT_SOFT_BLOCKED = CUSTOM_OPT_PREFIX + "soft_blocked"
     private const val CUSTOM_OPT_DYNAMIC_SHORTCUT = CUSTOM_OPT_PREFIX + "dynamic_shortcut"
     private const val CUSTOM_OPT_THUMBNAILS = CUSTOM_OPT_PREFIX + "thumbnails"
-    private const val CUSTOM_OPT_VFS_CACHING = CUSTOM_OPT_PREFIX + "vfs_caching"
     private const val CUSTOM_OPT_REPORT_USAGE = CUSTOM_OPT_PREFIX + "report_usage"
+    private const val CUSTOM_OPT_VFS_OPTIONS_PREFIX = CUSTOM_OPT_PREFIX + "vfs:"
 
     // Keep in sync with preferences_edit_remote.xml
     private const val DEFAULT_HARD_BLOCKED = false
     private const val DEFAULT_SOFT_BLOCKED = false
     private const val DEFAULT_DYNAMIC_SHORTCUT = false
     private const val DEFAULT_THUMBNAILS = true
-    private const val DEFAULT_VFS_CACHING = true
     private const val DEFAULT_REPORT_USAGE = false
 
     /**
@@ -41,7 +37,6 @@ object RcloneRpc {
 
         when (method) {
             "config/create", "config/delete", "config/update" -> RcloneConfig.notifyConfigChanged()
-            else -> Log.d(TAG, "No backup notification required for $method RPC call")
         }
 
         if (result.status != 200L) {
@@ -164,7 +159,7 @@ object RcloneRpc {
 
         // The RPC call will not clear out fs/vfs caches, so we'll do that manually here. Otherwise,
         // we might reference stale data if the user creates a new remote with the same name.
-        Rcbridge.rbCacheClearRemote("$remote:")
+        Rcbridge.rbCacheClearRemote("$remote:", true)
     }
 
     @Suppress("unused")
@@ -379,13 +374,23 @@ object RcloneRpc {
     }
 
     /** Directly and non-interactively set config key/value pairs for a remote. */
-    private fun setRemoteOptions(remote: String, options: Map<String, String>) {
+    private fun setRemoteOptions(remote: String, options: Map<String, String?>) {
+        // The RPC API can add and update keys, but cannot delete them. This is done before
+        // config/update because the RPC call will trigger the notification to the backup manager.
+        for ((k, v) in options.entries) {
+            if (v == null) {
+                RcloneConfig.deleteSectionKey(remote, k)
+            }
+        }
+
         invoke("config/update", JSONObject()
             .put("name", remote)
             .put("parameters", JSONObject()
                 .apply {
                     for ((k, v) in options.entries) {
-                        put(k, v)
+                        if (v != null) {
+                            put(k, v)
+                        }
                     }
                 }
             )
@@ -410,16 +415,20 @@ object RcloneRpc {
         val softBlocked: Boolean? = null,
         val dynamicShortcut: Boolean? = null,
         val thumbnails: Boolean? = null,
-        val vfsCaching: Boolean? = null,
         val reportUsage: Boolean? = null,
+        val vfsOptions: Map<String, String> = emptyMap(),
     ) {
         constructor(config: Map<String, String>) : this(
             hardBlocked = config[CUSTOM_OPT_HARD_BLOCKED]?.toBooleanStrictOrNull(),
             softBlocked = config[CUSTOM_OPT_SOFT_BLOCKED]?.toBooleanStrictOrNull(),
             dynamicShortcut = config[CUSTOM_OPT_DYNAMIC_SHORTCUT]?.toBooleanStrictOrNull(),
             thumbnails = config[CUSTOM_OPT_THUMBNAILS]?.toBooleanStrictOrNull(),
-            vfsCaching = config[CUSTOM_OPT_VFS_CACHING]?.toBooleanStrictOrNull(),
             reportUsage = config[CUSTOM_OPT_REPORT_USAGE]?.toBooleanStrictOrNull(),
+            vfsOptions = config
+                .asSequence()
+                .filter { it.key.startsWith(CUSTOM_OPT_VFS_OPTIONS_PREFIX) }
+                .map { it.key.substring(CUSTOM_OPT_VFS_OPTIONS_PREFIX.length) to it.value }
+                .toMap(),
         )
 
         fun toMap(): Map<String, String> = buildMap {
@@ -427,8 +436,8 @@ object RcloneRpc {
             softBlocked?.let { put(CUSTOM_OPT_SOFT_BLOCKED, it.toString()) }
             dynamicShortcut?.let { put(CUSTOM_OPT_DYNAMIC_SHORTCUT, it.toString()) }
             thumbnails?.let { put(CUSTOM_OPT_THUMBNAILS, it.toString()) }
-            vfsCaching?.let { put(CUSTOM_OPT_VFS_CACHING, it.toString()) }
             reportUsage?.let { put(CUSTOM_OPT_REPORT_USAGE, it.toString()) }
+            vfsOptions.mapKeysTo(this) { CUSTOM_OPT_VFS_OPTIONS_PREFIX + it.key }
         }
 
         val hardBlockedOrDefault: Boolean
@@ -439,14 +448,23 @@ object RcloneRpc {
             get() = dynamicShortcut ?: DEFAULT_DYNAMIC_SHORTCUT
         val thumbnailsOrDefault: Boolean
             get() = thumbnails ?: DEFAULT_THUMBNAILS
-        val vfsCachingOrDefault: Boolean
-            get() = vfsCaching ?: DEFAULT_VFS_CACHING
         val reportUsageOrDefault: Boolean
             get() = reportUsage ?: DEFAULT_REPORT_USAGE
     }
 
     fun setRemoteConfig(remote: String, config: RemoteConfig) {
-        setRemoteOptions(remote, config.toMap())
+        // Ensure we don't leave behind legacy options.
+        val updates = mutableMapOf<String, String?>()
+
+        remoteConfigsRaw[remote]
+            ?.asSequence()
+            ?.filter { (key, _) -> key.startsWith(CUSTOM_OPT_PREFIX) }
+            ?.map { (key, _) -> key to null }
+            ?.toMap(updates)
+
+        updates.putAll(config.toMap())
+
+        setRemoteOptions(remote, updates)
     }
 
     data class Usage(
@@ -477,5 +495,39 @@ object RcloneRpc {
             getOptLong("free"),
             getOptLong("objects"),
         )
+    }
+
+    val vfses: Array<String>
+        get() {
+            val output = invoke("vfs/list", JSONObject())
+            val vfses = output.optJSONArray("vfses") ?: return emptyArray()
+
+            return Array(vfses.length()) {
+                vfses.getString(it)
+            }
+        }
+
+    data class VfsQueueStats(
+        val inProgress: Int,
+        val pending: Int,
+    )
+
+    fun vfsQueueStats(vfs: String): VfsQueueStats {
+        val output = invoke("vfs/queue", JSONObject().put("fs", vfs))
+        val jsonQueue = output.getJSONArray("queue")
+        var inProgress = 0
+        var pending = 0
+
+        for (i in 0 until jsonQueue.length()) {
+            val jsonQueueItem = jsonQueue.getJSONObject(i)
+
+            if (jsonQueueItem.getBoolean("uploading")) {
+                inProgress += 1
+            } else {
+                pending += 1
+            }
+        }
+
+        return VfsQueueStats(inProgress, pending)
     }
 }

app/src/main/AndroidManifest.xml
@@ -57,22 +57,17 @@
             android:exported="false" />
 
         <service
-            android:name=".rclone.OpenFilesService"
+            android:name=".rclone.KeepAliveService"
             android:foregroundServiceType="specialUse"
             android:exported="false" />
 
         <receiver
-            android:name=".rclone.BackgroundUploadMonitorReceiver"
+            android:name=".rclone.ResumeUploadsBootReceiver"
             android:enabled="true"
             android:exported="true">
             <intent-filter>
                 <action android:name="android.intent.action.BOOT_COMPLETED" />
             </intent-filter>
         </receiver>
-
-        <service
-            android:name=".rclone.BackgroundUploadMonitorService"
-            android:foregroundServiceType="specialUse"
-            android:exported="false" />
     </application>
 </manifest>

app/src/main/java/com/chiller3/rsaf/Notifications.kt
@@ -10,41 +10,31 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
-import android.text.format.Formatter
-import com.chiller3.rsaf.rclone.VfsCache
-import kotlin.math.roundToInt
+import com.chiller3.rsaf.rclone.KeepAliveService
 
 class Notifications(private val context: Context) {
     companion object {
-        private const val CHANNEL_ID_OPEN_FILES = "open_files"
-        private const val CHANNEL_ID_BACKGROUND_UPLOADS = "background_uploads"
+        private const val CHANNEL_ID_KEEP_ALIVE = "keep_alive"
         private const val CHANNEL_ID_FAILURE = "failure"
 
-        private val LEGACY_CHANNEL_IDS = arrayOf<String>()
+        private val LEGACY_CHANNEL_IDS = arrayOf<String>(
+            "open_files",
+            "background_uploads",
+        )
 
-        const val ID_OPEN_FILES = -1
-        const val ID_BACKGROUND_UPLOADS = -2
+        const val ID_KEEP_ALIVE = -1
     }
 
     private val prefs = Preferences(context)
     private val notificationManager = context.getSystemService(NotificationManager::class.java)
 
-    /** Create a low priority notification channel for the open files notification. */
-    private fun createOpenFilesChannel() = NotificationChannel(
-        CHANNEL_ID_OPEN_FILES,
-        context.getString(R.string.notification_channel_open_files_name),
+    /** Create a low priority notification channel for the keep alive notification. */
+    private fun createKeepAliveChannel() = NotificationChannel(
+        CHANNEL_ID_KEEP_ALIVE,
+        context.getString(R.string.notification_channel_keep_alive_name),
         NotificationManager.IMPORTANCE_LOW,
     ).apply {
-        description = context.getString(R.string.notification_channel_open_files_desc)
-    }
-
-    /** Create a low priority notification channel for the background uploads notification. */
-    private fun createBackgroundUploadsChannel() = NotificationChannel(
-        CHANNEL_ID_BACKGROUND_UPLOADS,
-        context.getString(R.string.notification_channel_background_uploads_name),
-        NotificationManager.IMPORTANCE_LOW,
-    ).apply {
-        description = context.getString(R.string.notification_channel_background_uploads_desc)
+        description = context.getString(R.string.notification_channel_keep_alive_desc)
     }
 
     /** Create a high priority notification channel for failure alerts. */
@@ -63,60 +53,42 @@ class Notifications(private val context: Context) {
      */
     fun updateChannels() {
         notificationManager.createNotificationChannels(listOf(
-            createOpenFilesChannel(),
-            createBackgroundUploadsChannel(),
+            createKeepAliveChannel(),
             createFailureAlertsChannel(),
         ))
         LEGACY_CHANNEL_IDS.forEach { notificationManager.deleteNotificationChannel(it) }
     }
 
-    fun createOpenFilesNotification(count: Int): Notification {
-        val title = context.resources.getQuantityString(
-            R.plurals.notification_open_files_title,
-            count,
-            count,
-        )
+    fun createKeepAliveNotification(state: KeepAliveService.State): Notification {
+        val message = if (state.total == 0) {
+            context.getString(R.string.notification_keep_alive_cleanup_wait_desc)
+        } else {
+            buildString {
+                for ((resId, count) in arrayOf(
+                    R.plurals.notification_keep_alive_files_open_desc to state.open,
+                    R.plurals.notification_keep_alive_files_uploading_desc to
+                            state.syncUploading + state.asyncUploading,
+                    R.plurals.notification_keep_alive_files_pending_desc to state.asyncPending,
+                )) {
+                    if (count == 0) {
+                        continue
+                    } else if (isNotEmpty()) {
+                        append('\n')
+                    }
 
-        return Notification.Builder(context, CHANNEL_ID_OPEN_FILES).run {
-            setContentTitle(title)
-            setSmallIcon(R.drawable.ic_notifications)
-            setOngoing(true)
-            setOnlyAlertOnce(true)
-
-            // Inhibit 10-second delay when showing persistent notification
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                setForegroundServiceBehavior(Notification.FOREGROUND_SERVICE_IMMEDIATE)
+                    append(context.resources.getQuantityString(resId, count, count))
+                }
             }
-
-            build()
         }
-    }
 
-    fun createBackgroundUploadsNotification(progress: VfsCache.Progress): Notification {
-        val title = context.resources.getQuantityString(
-            R.plurals.notification_background_uploads_in_progress_title,
-            progress.count,
-            progress.count,
-        )
-
-        return Notification.Builder(context, CHANNEL_ID_BACKGROUND_UPLOADS).run {
-            setContentTitle(title)
+        return Notification.Builder(context, CHANNEL_ID_KEEP_ALIVE).run {
+            setContentTitle(context.getString(R.string.notification_keep_alive_title))
             setSmallIcon(R.drawable.ic_notifications)
             setOngoing(true)
             setOnlyAlertOnce(true)
 
-            val formattedBytesCurrent = Formatter.formatFileSize(context, progress.bytesCurrent)
-            val formattedBytesTotal = Formatter.formatFileSize(context, progress.bytesTotal)
-            setContentText("$formattedBytesCurrent / $formattedBytesTotal")
-
-            val normalizedBytesTotal = 1000
-            val normalizedBytesCurrent = if (progress.bytesTotal == 0L) {
-                0
-            } else {
-                (progress.bytesCurrent.toDouble() / progress.bytesTotal * normalizedBytesTotal)
-                    .roundToInt()
-            }
-            setProgress(normalizedBytesTotal, normalizedBytesCurrent, progress.bytesTotal == 0L)
+            setContentText(message)
+            style = Notification.BigTextStyle()
 
             // Inhibit 10-second delay when showing persistent notification
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {

app/src/main/java/com/chiller3/rsaf/Preferences.kt
@@ -50,8 +50,8 @@ class Preferences(private val context: Context) {
         const val PREF_ALLOW_LOCKED_ACCESS = "allow_locked_access"
         const val PREF_DYNAMIC_SHORTCUT = "dynamic_shortcut"
         const val PREF_THUMBNAILS = "thumbnails"
-        const val PREF_VFS_CACHING = "vfs_caching"
         const val PREF_REPORT_USAGE = "report_usage"
+        const val PREF_VFS_OPTIONS = "vfs_options"
 
         // Not associated with a UI preference
         const val PREF_DEBUG_MODE = "debug_mode"

app/src/main/java/com/chiller3/rsaf/dialog/AuthorizeDialogFragment.kt
@@ -48,13 +48,14 @@ open class AuthorizeDialogFragment : DialogFragment() {
 
         binding = DialogAuthorizeBinding.inflate(layoutInflater)
 
+        isCancelable = false
+
         val dialog = MaterialAlertDialogBuilder(requireContext())
             .setTitle(R.string.dialog_authorize_title)
             .setView(binding.root)
-            .setNegativeButton(R.string.dialog_action_cancel) { _, _ ->
+            .setNegativeButton(android.R.string.cancel) { _, _ ->
                 viewModel.cancel()
             }
-            .setCancelable(false)
             .create()
             .apply {
                 if (Preferences(requireContext()).dialogsAtBottom) {

commit 1234db4c9e5341810a18ebe297dd2aba69ef55b4

RcloneRpc: Avoid resetting all custom options when setting single option
The intention was to automatically remove all legacy custom options when
setting any new custom option. However, the incorrect conditional caused
all custom options to be reset to their defaults when changing a single
one. This was a regression introduced in commit
42e18b38d1cb67cdb0302220f331850eb373d6d4.

Fixes: #258

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/rsaf/dialog/VfsOptionsDialogFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2025-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -48,13 +48,11 @@ class VfsOptionsDialogFragment : DialogFragment() {
 
     private lateinit var binding: DialogVfsOptionsBinding
     private lateinit var remote: String
-    private lateinit var config: RcloneRpc.RemoteConfig
     private var overrides: Map<String, String>? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val arguments = requireArguments()
         remote = arguments.getString(ARG_REMOTE)!!
-        config = RcloneRpc.remoteConfigs[remote]!!
 
         binding = DialogVfsOptionsBinding.inflate(layoutInflater)
 
@@ -101,6 +99,8 @@ class VfsOptionsDialogFragment : DialogFragment() {
         }
 
         if (savedInstanceState == null) {
+            val config = RcloneRpc.remoteConfigs[remote]!!
+
             binding.text.setText(buildString {
                 for ((key, value) in config.vfsOptions) {
                     if (isNotEmpty()) {
@@ -196,7 +196,7 @@ class VfsOptionsDialogFragment : DialogFragment() {
     }
 
     private fun save(reload: Boolean) {
-        RcloneRpc.setRemoteConfig(remote, config.copy(vfsOptions = overrides!!))
+        RcloneRpc.setRemoteConfig(remote, RcloneRpc.RemoteConfig(vfsOptions = overrides!!))
 
         if (reload) {
             Rcbridge.rbCacheClearRemote("$remote:", false)

app/src/main/java/com/chiller3/rsaf/rclone/RcloneRpc.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023-2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -26,6 +26,15 @@ object RcloneRpc {
     private const val DEFAULT_DYNAMIC_SHORTCUT = false
     private const val DEFAULT_THUMBNAILS = true
     private const val DEFAULT_REPORT_USAGE = false
+
+    private fun isKnownKey(key: String) =
+        !key.startsWith(CUSTOM_OPT_PREFIX)
+                || key == CUSTOM_OPT_HARD_BLOCKED
+                || key == CUSTOM_OPT_SOFT_BLOCKED
+                || key == CUSTOM_OPT_DYNAMIC_SHORTCUT
+                || key == CUSTOM_OPT_THUMBNAILS
+                || key == CUSTOM_OPT_REPORT_USAGE
+                || key.startsWith(CUSTOM_OPT_VFS_OPTIONS_PREFIX)
 
     /**
      * Perform an rclone RPC call.
@@ -452,12 +461,12 @@ object RcloneRpc {
     }
 
     fun setRemoteConfig(remote: String, config: RemoteConfig) {
-        // Ensure we don't leave behind legacy options.
         val updates = mutableMapOf<String, String?>()
 
+        // Ensure we don't leave behind legacy options.
         remoteConfigsRaw[remote]
             ?.asSequence()
-            ?.filter { (key, _) -> key.startsWith(CUSTOM_OPT_PREFIX) }
+            ?.filter { (key, _) -> !isKnownKey(key) }
             ?.associateTo(updates) { (key, _) -> key to null }
 
         updates.putAll(config.toMap())