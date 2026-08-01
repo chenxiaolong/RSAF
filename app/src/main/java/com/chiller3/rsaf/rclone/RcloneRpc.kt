chenxiaolong/RSAF

commit 1234db4c9e5341810a18ebe297dd2aba69ef55b4

RcloneRpc: Avoid resetting all custom options when setting single option
The intention was to automatically remove all legacy custom options when
setting any new custom option. However, the incorrect conditional caused
all custom options to be reset to their defaults when changing a single
one. This was a regression introduced in commit
42e18b38d1cb67cdb0302220f331850eb373d6d4.

Fixes: #258

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/rsaf/rclone/RcloneRpc.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023-2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -26,6 +26,15 @@ object RcloneRpc {
     private const val DEFAULT_DYNAMIC_SHORTCUT = false
     private const val DEFAULT_THUMBNAILS = true
     private const val DEFAULT_REPORT_USAGE = false
+
+    private fun isKnownKey(key: String) =
+        !key.startsWith(CUSTOM_OPT_PREFIX)
+                || key == CUSTOM_OPT_HARD_BLOCKED
+                || key == CUSTOM_OPT_SOFT_BLOCKED
+                || key == CUSTOM_OPT_DYNAMIC_SHORTCUT
+                || key == CUSTOM_OPT_THUMBNAILS
+                || key == CUSTOM_OPT_REPORT_USAGE
+                || key.startsWith(CUSTOM_OPT_VFS_OPTIONS_PREFIX)
 
     /**
      * Perform an rclone RPC call.
@@ -452,12 +461,12 @@ object RcloneRpc {
     }
 
     fun setRemoteConfig(remote: String, config: RemoteConfig) {
-        // Ensure we don't leave behind legacy options.
         val updates = mutableMapOf<String, String?>()
 
+        // Ensure we don't leave behind legacy options.
         remoteConfigsRaw[remote]
             ?.asSequence()
-            ?.filter { (key, _) -> key.startsWith(CUSTOM_OPT_PREFIX) }
+            ?.filter { (key, _) -> !isKnownKey(key) }
             ?.associateTo(updates) { (key, _) -> key to null }
 
         updates.putAll(config.toMap())

app/src/main/java/com/chiller3/rsaf/dialog/VfsOptionsDialogFragment.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2025-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -48,13 +48,11 @@ class VfsOptionsDialogFragment : DialogFragment() {
 
     private lateinit var binding: DialogVfsOptionsBinding
     private lateinit var remote: String
-    private lateinit var config: RcloneRpc.RemoteConfig
     private var overrides: Map<String, String>? = null
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         val arguments = requireArguments()
         remote = arguments.getString(ARG_REMOTE)!!
-        config = RcloneRpc.remoteConfigs[remote]!!
 
         binding = DialogVfsOptionsBinding.inflate(layoutInflater)
 
@@ -101,6 +99,8 @@ class VfsOptionsDialogFragment : DialogFragment() {
         }
 
         if (savedInstanceState == null) {
+            val config = RcloneRpc.remoteConfigs[remote]!!
+
             binding.text.setText(buildString {
                 for ((key, value) in config.vfsOptions) {
                     if (isNotEmpty()) {
@@ -196,7 +196,7 @@ class VfsOptionsDialogFragment : DialogFragment() {
     }
 
     private fun save(reload: Boolean) {
-        RcloneRpc.setRemoteConfig(remote, config.copy(vfsOptions = overrides!!))
+        RcloneRpc.setRemoteConfig(remote, RcloneRpc.RemoteConfig(vfsOptions = overrides!!))
 
         if (reload) {
             Rcbridge.rbCacheClearRemote("$remote:", false)

commit 0d293304e81e71913ce942c9031f0f268a2ed76b

Fix custom VFS options not being able to be removed
Existing VFS options always got treated as known keys and thus, were
never reset. This was a regression introduced in commit
1234db4c9e5341810a18ebe297dd2aba69ef55b4.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/rsaf/rclone/RcloneRpc.kt
@@ -526,7 +526,7 @@ object RcloneRpc {
         val dynamicShortcut: Boolean? = null,
         val thumbnails: Boolean? = null,
         val reportUsage: Boolean? = null,
-        val vfsOptions: Map<String, String> = emptyMap(),
+        val vfsOptions: Map<String, String>? = null,
     ) {
         constructor(config: Map<String, String>) : this(
             hardBlocked = config[CUSTOM_OPT_HARD_BLOCKED]?.toBooleanStrictOrNull(),
@@ -537,7 +537,8 @@ object RcloneRpc {
             vfsOptions = config
                 .asSequence()
                 .filter { it.key.startsWith(CUSTOM_OPT_VFS_OPTIONS_PREFIX) }
-                .associate { it.key.substring(CUSTOM_OPT_VFS_OPTIONS_PREFIX.length) to it.value },
+                .associate { it.key.substring(CUSTOM_OPT_VFS_OPTIONS_PREFIX.length) to it.value }
+                .takeIf { it.isNotEmpty() },
         )
 
         fun toMap(): Map<String, String> = buildMap {
@@ -546,7 +547,17 @@ object RcloneRpc {
             dynamicShortcut?.let { put(CUSTOM_OPT_DYNAMIC_SHORTCUT, it.toString()) }
             thumbnails?.let { put(CUSTOM_OPT_THUMBNAILS, it.toString()) }
             reportUsage?.let { put(CUSTOM_OPT_REPORT_USAGE, it.toString()) }
-            vfsOptions.mapKeysTo(this) { CUSTOM_OPT_VFS_OPTIONS_PREFIX + it.key }
+            vfsOptions?.let { it.mapKeysTo(this) { e -> CUSTOM_OPT_VFS_OPTIONS_PREFIX + e.key } }
+        }
+
+        fun changesKey(key: String): Boolean = when {
+            key == CUSTOM_OPT_HARD_BLOCKED -> hardBlocked != null
+            key == CUSTOM_OPT_SOFT_BLOCKED -> softBlocked != null
+            key == CUSTOM_OPT_DYNAMIC_SHORTCUT -> dynamicShortcut != null
+            key == CUSTOM_OPT_THUMBNAILS -> thumbnails != null
+            key == CUSTOM_OPT_REPORT_USAGE -> reportUsage != null
+            key.startsWith(CUSTOM_OPT_VFS_OPTIONS_PREFIX) -> vfsOptions != null
+            else -> false
         }
 
         val hardBlockedOrDefault: Boolean
@@ -559,15 +570,17 @@ object RcloneRpc {
             get() = thumbnails ?: DEFAULT_THUMBNAILS
         val reportUsageOrDefault: Boolean
             get() = reportUsage ?: DEFAULT_REPORT_USAGE
+        val vfsOptionsOrDefault: Map<String, String>
+            get() = vfsOptions ?: emptyMap()
     }
 
     fun setRemoteConfig(remote: String, config: RemoteConfig) {
         val updates = mutableMapOf<String, String?>()
 
-        // Ensure we don't leave behind legacy options.
+        // Explicitly unset any legacy keys and keys that are going to be changed by this operation.
         remoteConfigsRaw[remote]
             ?.asSequence()
-            ?.filter { (key, _) -> !isKnownKey(key) }
+            ?.filter { (key, _) -> !isKnownKey(key) || config.changesKey(key) }
             ?.associateTo(updates) { (key, _) -> key to null }
 
         updates.putAll(config.toMap())

app/src/main/java/com/chiller3/rsaf/rclone/VfsCache.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2024-2025 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2024-2026 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -159,7 +159,7 @@ object VfsCache {
             }
 
             val vfsOptions = try {
-                getVfsOptions(config.vfsOptions)
+                getVfsOptions(config.vfsOptionsOrDefault)
             } catch (e: Exception) {
                 Log.w(TAG, "Invalid VFS option overrides for remote: $remote", e)
                 continue

app/src/main/java/com/chiller3/rsaf/settings/EditRemoteScreen.kt
@@ -415,7 +415,7 @@ private fun EditRemoteContent(
     if (showVfsOptionsDialog) {
         VfsOptionsDialog(
             remote = remote,
-            initialOptions = state.config?.vfsOptions!!,
+            initialOptions = state.config!!.vfsOptionsOrDefault,
             onSelect = { options, reload ->
                 onVfsOptionsChange(options, reload)
                 showVfsOptionsDialog = false

app/src/androidTest/java/com/chiller3/rsaf/RcloneProviderTest.kt
@@ -347,7 +347,7 @@ class RcloneProviderTest {
 
     private fun setVfsCaching(enabled: Boolean?) {
         val config = RcloneRpc.remoteConfigs[remote]!!
-        val vfsOptions = config.vfsOptions.toMutableMap()
+        val vfsOptions = config.vfsOptionsOrDefault.toMutableMap()
 
         when (enabled) {
             true -> vfsOptions["vfs_cache_mode"] = "writes"